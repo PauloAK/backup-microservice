d2ea597bcb2c3cb24b7630b417b1e5927587c04a
Paulo Kramer <paulo_kramer@hotmail.com>
2021-03-02 12:28:50 -0300

    Backup process adjustments

--- services/backup.service.js
+++ services/backup.service.js
@@ -8,12 +8,12 @@ module.exports = {
     name: "backup",
 
     actions: {
-        setupPath(ctx) {
+        async setupPath(ctx) {
             let fileExtension = "tar.gz";
             let timeFormat = dateformat(new Date, "yyyymmdd-HHMMss");
 
-            let dir = ctx.backup_folder + `/${ctx.app.name}`;
-            let path = `${dir}/${ctx.app.name}_${timeFormat}_${ctx.type}.${fileExtension}`;
+            let dir = /*ctx.params.backup_folder*/ '/tmp' + `/${ctx.params.app.name}`;
+            let path = `${dir}/${ctx.params.app.name}_${timeFormat}_${ctx.params.type}.${fileExtension}`;
 
             if (!fs.existsSync(dir)){
                 fs.mkdirSync(dir);
@@ -23,49 +23,49 @@ module.exports = {
             return path;
         },
 
-        directory(ctx) {
+        async directory(ctx) {
             this.logger.info("Backuping directories");
             let path = await ctx.call("backup.setupPath", {
-                app: ctx.app,
+                app: ctx.params.app,
                 type: "directories"
             });
 
             await tar.create({
                 gzip: true,
                 file: path
-            }, ctx.directories).then( tar => {
+            }, ctx.params.app.directories).then( tar => {
                 this.logger.info("Directories backup completed.");
             });
 
             return path;
         },
 
-        database(ctx) {
+        async database(ctx) {
             this.logger.info("Backuping databases");
             // TODO
         },
 
-        process(ctx) {
+        async process(ctx) {
             let directoryBackupPath = await ctx.call("backup.directory", {
-                app: ctx.app
+                app: ctx.params.app
             });
 
-            let databaseBackupPath = await ctx.call("backup.database", {
-                app: ctx.app
-            });
+            /*let databaseBackupPath = await ctx.call("backup.database", {
+                app: ctx.params.app
+            });*/
 
             this.logger.info("Merging backups...");
             let path = await ctx.call("backup.setupPath", {
-                app: ctx.app,
+                app: ctx.params.app,
                 type: "all"
             });
 
             await tar.create({
                 gzip: true,
                 file: path
-            }, ctx.directories).then( tar => {
+            }, [ directoryBackupPath /*, databaseBackupPath*/ ]).then( async tar => {
                 fs.unlinkSync(directoryBackupPath);
-                fs.unlinkSync(databaseBackupPath);
+                //fs.unlinkSync(databaseBackupPath);
                 this.logger.info(`Merge completed. Full backup available at: ${path}`);
             });
         }
@@ -73,12 +73,12 @@ module.exports = {
 
     events: {
         "init.backup": {
-            handler(ctx) {
+            async handler(ctx) {
                 this.logger.info("Backup proccess started");
                 let config = JSON.parse(fs.readFileSync('./backup-config.json'));
                 this.logger.info("Config loaded...");
 
-                config.apps.forEach(function(app){
+                config.apps.forEach(async app => {
                     this.logger.info(`[${app.name}] Starting`);
                     await ctx.call("backup.process", {
                         app: app
